b053db9fd7aaf8bb14b7da9eb1bfbe31fac5b55b
AltNtv <somealternativemail@gmail.com>
2022-07-08 11:56:37 +0300

    Gravity and Ground Detection
    Worked with PlayerCharacterController. Added collider points onto the player. Casted sphere to detect collision with ground. Added simple gravity. Looks cool so far!

--- Revenge of Nature/Assets/_Scripts/Player/PlayerCharacterController.cs
+++ Revenge of Nature/Assets/_Scripts/Player/PlayerCharacterController.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 
 [RequireComponent(typeof(PlayerInputHandler), typeof(CharacterController))]
-public class PlayerCharacterController : MonoBehaviour
+internal sealed class PlayerCharacterController : MonoBehaviour
 {
+    [Tooltip("Character controller top hemisphere point gameObject")]
+    [SerializeField] private GameObject m_topHemispherePoint;
+
+    [Tooltip("Character controller bottom hemisphere point gameObject")]
+    [SerializeField] private GameObject m_bottomHemispherePoint;
+
+    [Tooltip("Character controller center point gameObject")]
+    [SerializeField] private GameObject m_centerPoint;
+
+    [Tooltip("Field in inspector for reference.")]
+    [SerializeField] private Camera m_playerCamera;
+
+    [Tooltip("Player movement speed.")]
+    [SerializeField] private float m_moveSpeed;
+
+    [Tooltip("Layer mask for ground check.")]
+    [SerializeField] private LayerMask m_groundCheckLayerMask;
+
     private PlayerInputHandler m_playerInput;
     private CharacterController m_characterController;
 
-
-    private float m_playerSpeed = 5f;
+    private bool m_isGrounded;
 
 
     private void Start()
@@ -19,7 +36,51 @@ public class PlayerCharacterController : MonoBehaviour
 
     private void Update()
     {
-        Vector3 direction = m_playerInput.GetMoveDirection();
-        m_characterController.Move(direction * (Time.deltaTime * m_playerSpeed));
+        // Rotate player around Y axis
+        float horizontalLook = m_playerInput.GetHorizontalLookAxis();
+        transform.rotation = Quaternion.Euler(0f, horizontalLook, 0f);
+
+        // Rotate camera around X axis
+        float verticalLook = m_playerInput.GetVerticalLookAxis();
+        m_playerCamera.transform.localRotation = Quaternion.Euler(-verticalLook, 0f, 0f);
+
+        // Get input velocity vector and transform it into world space
+        Vector3 velocity = m_playerInput.GetMoveDirection() * (m_moveSpeed * Time.deltaTime);
+        Vector3 velocityInWorldSpace = transform.TransformVector(velocity);
+
+        if (m_isGrounded)
+        {
+            velocityInWorldSpace.y = -0.5f;
+        }
+        else
+        {
+            velocityInWorldSpace.y += -9.81f * Time.deltaTime;
+        }
+
+        m_characterController.Move(velocityInWorldSpace);
     }
+
+    private bool CheckGround()
+    {
+        // Cast bottom hemisphere down to check collisions
+        RaycastHit hit;
+        if (Physics.SphereCast(m_bottomHemispherePoint.transform.position, m_characterController.radius, Vector3.down, out hit,
+            0.2f, m_groundCheckLayerMask, QueryTriggerInteraction.Ignore))
+        {
+            // Don't consider bad angles and sharp slopes as ground
+            if (Vector3.Dot(hit.normal, Vector3.up) > 0f && Vector3.Angle(hit.normal, Vector3.up) < m_characterController.slopeLimit)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // I used it to look how hemisphere points are positioned
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(m_topHemispherePoint.transform.position, m_characterController.radius);
+    //    Gizmos.DrawWireSphere(m_bottomHemispherePoint.transform.position, m_characterController.radius);
+    //}
 }
--- Revenge of Nature/Assets/_Scripts/Player/PlayerInputHandler.cs
+++ Revenge of Nature/Assets/_Scripts/Player/PlayerInputHandler.cs
@@ -2,19 +2,32 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class PlayerInputHandler : MonoBehaviour
+// If player presses any button inside the game, it should be added to the InputMap which is located in Resources/Input
+// Then should to subscribe in OnEnable() on events which are in need. Don't forget to unsibscribe in OnDisable() method!
+
+internal sealed class PlayerInputHandler : MonoBehaviour
 {
+    [Tooltip("Invert mouse X Axis")]
+    [SerializeField] private bool m_invertXAxis = false;
+
+    [Tooltip("Invert mouse Y Axis")]
+    [SerializeField] private bool m_invertYAxis = false;
+
+    [Tooltip("Mouse Sensitivity X Axis")]
+    [SerializeField] private float m_mouseSensitivityX = 1f;
+
+    [Tooltip("Mouse Sensitivity Y Axis")]
+    [SerializeField] private float m_mouseSensitivityY = 1f;
+
     private InputMap m_inputMap;
     private InputMap.PlayerInputActions m_playerInputActions;
 
-    private float m_movementHorizontalInput;
-    private float m_movementVerticalInput;
-    private float m_rotationDeltaHorizontalInput;
-    private float m_rotationDeltaVerticalInput;
-
-    [SerializeField] private float mouseSensitivityX = 1f;
-    [SerializeField] private float mouseSensitivityY = 1f;
-
+    private float m_movementHorizontalInput = 0f;
+    private float m_movementVerticalInput = 0f;
+    private float m_rotationDeltaHorizontalInput = 0f;
+    private float m_rotationDeltaVerticalInput = 0f;
+    private float m_rotationHorizontal = 0f;
+    private float m_rotationVertical = 0f;
 
 
     private void Awake()
@@ -23,6 +36,7 @@ public class PlayerInputHandler : MonoBehaviour
         m_playerInputActions = m_inputMap.PlayerInput;
     }
 
+    // Subscribing on events
     private void OnEnable()
     {
         m_inputMap.Enable();
@@ -38,7 +52,7 @@ public class PlayerInputHandler : MonoBehaviour
         m_playerInputActions.RotationDeltaHorizontal.performed += OnRotationDeltaHorizontalChanged;
         m_playerInputActions.RotationDeltaVertical.performed += OnRotationDeltaVerticalChanged;
     }
-
+    // Unsubscribing from events
     private void OnDisable()
     {
         m_playerInputActions.MovementHorizontal.started -= OnMovementChangedHorizontal;
@@ -59,22 +73,58 @@ public class PlayerInputHandler : MonoBehaviour
     {
         m_movementHorizontalInput = context.ReadValue<float>();
     }
+
     private void OnMovementChangedVertical(InputAction.CallbackContext context)
     {
         m_movementVerticalInput = context.ReadValue<float>();
     }
+
     private void OnRotationDeltaHorizontalChanged(InputAction.CallbackContext context)
     {
         m_rotationDeltaHorizontalInput = context.ReadValue<float>();
+
+        // Multiply input value by mouse sensitivity
+        m_rotationHorizontal += m_rotationDeltaHorizontalInput * m_mouseSensitivityX;
+
+        // Invert rotation if needed
+        if (m_invertXAxis)
+        {
+            m_rotationHorizontal *= -1f;
+        }
     }
+
     private void OnRotationDeltaVerticalChanged(InputAction.CallbackContext context)
     {
         m_rotationDeltaVerticalInput = context.ReadValue<float>();
+
+        // Multiply input value by mouse sensitivity
+        m_rotationVertical += m_rotationDeltaVerticalInput * m_mouseSensitivityY;
+
+        // Invert rotation if needed
+        if (m_invertYAxis)
+        {
+            m_rotationVertical *= -1f;
+        }
+
+        // Limit vertical rotation
+        m_rotationVertical = Mathf.Clamp(m_rotationVertical, -89f, 89f);
     }
 
     public Vector3 GetMoveDirection()
     {
         Vector3 direction = new Vector3(m_movementHorizontalInput, 0f, m_movementVerticalInput);
+
+        // Limit direction magnitude to 1f
         return Vector3.ClampMagnitude(direction, 1f);
+    }
+
+    public float GetHorizontalLookAxis()
+    {
+        return m_rotationHorizontal;
+    }
+
+    public float GetVerticalLookAxis()
+    {
+        return m_rotationVertical;
     }
 }
