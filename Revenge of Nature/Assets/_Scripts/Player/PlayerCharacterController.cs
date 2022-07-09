8b6988690bcbc8a726775339e742ebeb59100e14
AltNtv <somealternativemail@gmail.com>
2022-07-09 21:51:06 +0300

    Gravity applied
    I struggle a lot with this character controller. I had some bugs which are so stupid idk. Still got to re-think this system of movement but let it be for today.

--- Revenge of Nature/Assets/_Scripts/Player/PlayerCharacterController.cs
+++ Revenge of Nature/Assets/_Scripts/Player/PlayerCharacterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerInputHandler), typeof(CharacterController))]
 internal sealed class PlayerCharacterController : MonoBehaviour
 {
+    [Header("References")]
+    [Tooltip("Field in inspector for reference.")]
+    [SerializeField] private Camera m_playerCamera;
+
     [Tooltip("Character controller top hemisphere point gameObject")]
     [SerializeField] private GameObject m_topHemispherePoint;
 
@@ -13,19 +17,32 @@ internal sealed class PlayerCharacterController : MonoBehaviour
     [Tooltip("Character controller center point gameObject")]
     [SerializeField] private GameObject m_centerPoint;
 
-    [Tooltip("Field in inspector for reference.")]
-    [SerializeField] private Camera m_playerCamera;
-
+    [Header("General")]
     [Tooltip("Player movement speed.")]
-    [SerializeField] private float m_moveSpeed;
+    [SerializeField] private float m_moveSpeed = 10f;
+
+    [Tooltip("Acceleration speed in air.")]
+    [SerializeField] private float m_accelerationSpeedInAir = 25f;
+
+    [Tooltip("Max speed in air."), SerializeField]
+    private float m_maxSpeedInAir = 10f;
 
     [Tooltip("Layer mask for ground check.")]
     [SerializeField] private LayerMask m_groundCheckLayerMask;
 
+    [Tooltip("Gravity force defines how fast player is falling.")]
+    [SerializeField] private float m_gravityForce;
+
+    [Tooltip("Sharpness of movement when player is on ground. Bigger value means sharper movement.")]
+    [SerializeField] private float m_movementSharpness = 15f;
+
     private PlayerInputHandler m_playerInput;
     private CharacterController m_characterController;
 
+    private Vector3 m_playerVelocity = Vector3.zero;
+    private Vector3 m_groundNormal;
     private bool m_isGrounded;
+
 
 
     private void Start()
@@ -36,6 +53,70 @@ internal sealed class PlayerCharacterController : MonoBehaviour
 
     private void Update()
     {
+        ApplyRotation();
+        CheckGround();
+
+        // Get input velocity vector and transform it into world space
+        Vector3 movementInput = m_playerInput.GetMoveDirection();
+        Vector3 worldspaceInput = transform.TransformVector(movementInput);
+
+        if (m_isGrounded)
+        {
+            // Get reoriented velocity that is tangent to a given slope
+            worldspaceInput = Vector3.Cross(m_groundNormal, Vector3.Cross(worldspaceInput, Vector3.up));
+
+            Vector3 targetVelocity = worldspaceInput * m_moveSpeed;
+
+            // Interpolate movement
+            m_playerVelocity = Vector3.Lerp(m_playerVelocity, targetVelocity, m_movementSharpness * Time.deltaTime);
+
+            m_playerVelocity.y -= 0.1f;
+        }
+
+        else
+        {
+            m_playerVelocity += worldspaceInput * (m_accelerationSpeedInAir * Time.deltaTime);
+
+            // Remember vertical velocity
+            float verticalVelocity = m_playerVelocity.y;
+            Vector3 horizontalVelocity = Vector3.ProjectOnPlane(m_playerVelocity, Vector3.up);
+
+
+            // Clamp horizontal velocity
+            horizontalVelocity = Vector3.ClampMagnitude(horizontalVelocity, m_maxSpeedInAir);
+
+            m_playerVelocity = horizontalVelocity + (Vector3.up * verticalVelocity);
+            m_playerVelocity += Vector3.down * m_gravityForce * Time.deltaTime;
+        }
+
+        m_characterController.Move(m_playerVelocity * Time.deltaTime);
+    }
+
+    // Check ground sets m_isGrounded value
+    private void CheckGround()
+    {
+        // Cast bottom hemisphere down to check collisions
+        RaycastHit hit;
+        if (Physics.SphereCast(m_bottomHemispherePoint.transform.position, m_characterController.radius, Vector3.down, out hit,
+            0.2f, m_groundCheckLayerMask, QueryTriggerInteraction.Ignore))
+        {
+            m_groundNormal = hit.normal;
+
+            // Consider hit as valid ground if only ground normal goes up
+            // and ground slope is not more than character controller slope limit
+            if (Vector3.Dot(hit.normal, Vector3.up) > 0f && Vector3.Angle(hit.normal, Vector3.up) < m_characterController.slopeLimit)
+            {
+                m_isGrounded = true;
+            }
+            else
+            {
+                m_isGrounded = false;
+            }
+        }
+    }
+
+    private void ApplyRotation()
+    {
         // Rotate player around Y axis
         float horizontalLook = m_playerInput.GetHorizontalLookAxis();
         transform.rotation = Quaternion.Euler(0f, horizontalLook, 0f);
@@ -43,44 +124,5 @@ internal sealed class PlayerCharacterController : MonoBehaviour
         // Rotate camera around X axis
         float verticalLook = m_playerInput.GetVerticalLookAxis();
         m_playerCamera.transform.localRotation = Quaternion.Euler(-verticalLook, 0f, 0f);
-
-        // Get input velocity vector and transform it into world space
-        Vector3 velocity = m_playerInput.GetMoveDirection() * (m_moveSpeed * Time.deltaTime);
-        Vector3 velocityInWorldSpace = transform.TransformVector(velocity);
-
-        if (m_isGrounded)
-        {
-            velocityInWorldSpace.y = -0.5f;
-        }
-        else
-        {
-            velocityInWorldSpace.y += -9.81f * Time.deltaTime;
-        }
-
-        m_characterController.Move(velocityInWorldSpace);
     }
-
-    private bool CheckGround()
-    {
-        // Cast bottom hemisphere down to check collisions
-        RaycastHit hit;
-        if (Physics.SphereCast(m_bottomHemispherePoint.transform.position, m_characterController.radius, Vector3.down, out hit,
-            0.2f, m_groundCheckLayerMask, QueryTriggerInteraction.Ignore))
-        {
-            // Don't consider bad angles and sharp slopes as ground
-            if (Vector3.Dot(hit.normal, Vector3.up) > 0f && Vector3.Angle(hit.normal, Vector3.up) < m_characterController.slopeLimit)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // I used it to look how hemisphere points are positioned
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawWireSphere(m_topHemispherePoint.transform.position, m_characterController.radius);
-    //    Gizmos.DrawWireSphere(m_bottomHemispherePoint.transform.position, m_characterController.radius);
-    //}
 }
